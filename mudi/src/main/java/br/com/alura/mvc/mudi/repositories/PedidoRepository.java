017f6dbe7d308c0a94fd91cdaadc07551878f4d9
Ravi Assis <ravi.g.assis@gmail.com>
2021-11-12 08:56:34 -0300

    Applying caching

--- mudi/src/main/java/br/com/alura/mvc/mudi/repositories/PedidoRepository.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/repositories/PedidoRepository.java
@@ -2,6 +2,7 @@ package br.com.alura.mvc.mudi.repositories;
 
 import br.com.alura.mvc.mudi.model.Pedido;
 import br.com.alura.mvc.mudi.model.StatusPedido;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+    @Cacheable("books")
     List<Pedido> findByStatus(StatusPedido aguardando, Pageable page);
 
     List<Pedido> findByUser_Username(String username);
